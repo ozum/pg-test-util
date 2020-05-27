058c68630b23b21bacaa20e89e61544697855f3f
Özüm Eldoğan <ozum@ozum.net>
2020-05-27 16:09:00 +0300

    fix: export additional types
    fix #3

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import PgTestUtil from "./pg-test-util";
 
+export * from "./types";
+export { PgTestUtilConstructorArgs } from "./pg-test-util";
+export { default as Database, DatabaseConstructorArgs } from "./database";
+
 export default PgTestUtil;
